6f02c731b34aee5a1e026ff3e102f52317e48134
bettercodevivek <viveksingh0520@gmail.com>
2024-03-03 00:41:58 +0530

    updation in skills file

--- 01 react-practice/src/Skills.jsx
+++ 01 react-practice/src/Skills.jsx
@@ -6,7 +6,39 @@ function Skills(){
         <h1>My Skills</h1>
         <div className='skills-container'>
             <div className='skill-item'>
-                <img src=" " alt=" "></img>
+                <img className="icon" src="src\assets\git-svgrepo-com.svg " alt="GIT"></img>
+                </div>
+
+                <div className='skill-item'>
+                <img className="icon" src="src\assets\python-svgrepo-com.svg " alt="GIT"></img>
+                </div>
+
+                <div className='skill-item'>
+                <img className="icon" src="src\assets\cpp-svgrepo-com.svg " alt="GIT"></img>
+                </div>
+
+                <div className='skill-item'>
+                <img className="icon" src="src\assets\css-3-svgrepo-com.svg " alt="GIT"></img>
+                </div>
+
+                <div className='skill-item'>
+                <img className="icon" src="src\assets\html-5-svgrepo-com.svg " alt="GIT"></img>
+                </div>
+
+                <div className='skill-item'>
+                <img className="icon" src="src\assets\java-svgrepo-com.svg " alt="GIT"></img>
+                </div>
+
+                <div className='skill-item'>
+                <img className="icon" src="src\assets\js-svgrepo-com.svg " alt="GIT"></img>
+                </div>
+
+                <div className='skill-item'>
+                <img className="icon" src="src\assets\node-js-svgrepo-com.svg " alt="GIT"></img>
+                </div>
+
+                <div className='skill-item'>
+                <img className="icon" src="src\assets\react-svgrepo-com.svg " alt="GIT"></img>
                 </div>
         </div>
     </div>
