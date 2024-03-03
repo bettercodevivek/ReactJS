555f1ccdf93508e66718ca90cbdc6ca942db7478
bettercodevivek <viveksingh0520@gmail.com>
2024-03-03 15:19:00 +0530

    Updation in footer file

---
+++ 01 react-practice/src/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer(){
+    return(
+       <div className='Footer-mainpage'>
+        <h1>Get In Touch</h1>
+       </div>
+    );
+}
+
+export default Footer;
