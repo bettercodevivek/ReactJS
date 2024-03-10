1569d1d463869c42555bd3886a13fed7d82ecda1
bettercodevivek <viveksingh0520@gmail.com>
2024-03-10 23:53:55 +0530

    updation

--- 01 react-practice/src/App.jsx
+++ 01 react-practice/src/App.jsx
@@ -13,7 +13,7 @@ function App(){
       <div className="introduction-container">
      <h1 className="introduction-heading-1">Hello, I am 'Your-Name'.</h1>
      <h3 className="introduction-heading-3">I am a FrontEnd Developer.</h3>
-     <h5 className="introduction-heading-5">Passionate software artisan skilled in full-stack development, fluent in diverse coding languages.<br></br> A tenacious problem solver, committed to crafting elegant and scalable solutions for a seamless user experience.</h5>
+     <h5 className="introduction-heading-5">Passionate software artisan skilled in FrontEnd development, fluent in diverse coding languages.<br></br> A tenacious problem solver, committed to crafting elegant and scalable solutions for a seamless user experience.</h5>
      <button id="hire-button">Hire Me!</button>
      </div>
     </div>
