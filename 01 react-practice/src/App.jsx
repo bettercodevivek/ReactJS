4d946e61bd8f3ffe90af3254a1bb0fe9a9e8dccb
bettercodevivek <viveksingh0520@gmail.com>
2024-04-11 17:53:03 +0530

    updation

--- 01 react-practice/src/App.jsx
+++ 01 react-practice/src/App.jsx
@@ -1,35 +1,10 @@
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
 import Navbar from "./navbar";
 import About from "./About";
 import Skills from "./Skills";
 import Projects from "./Projects";
 import Footer from "./Footer";
 import "./App.css";
-
-const footer1 = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bettercodevivek/ReactJS')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
-}
 
 function App(){
   return (
@@ -47,24 +22,6 @@ function App(){
     <Skills />
     <Projects />
     <Footer />
-    <div className="credits-container">
-    <a href="https://github.com/bettercodevivek/ReactJS">
-          <div>Designed &amp; Built by Vivek Singh</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
-        </a>
-    </div>
     </div>
   );
 }
