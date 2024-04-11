1480b3c30796842911d5cdaacdb13ab39a97b848
bettercodevivek <viveksingh0520@gmail.com>
2024-04-11 17:18:18 +0530

    updation

--- 01 react-practice/src/App.jsx
+++ 01 react-practice/src/App.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import Navbar from "./navbar";
 import About from "./About";
 import Skills from "./Skills";
 import Projects from "./Projects";
 import Footer from "./Footer";
 import "./App.css";
+
+const Footer = () => {
+  const [githubInfo, setGitHubInfo] = useState({
+    stars: null,
+    forks: null,
+  });
+
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'production') {
+      return;
+    }
+    fetch('https://api.github.com/repos/bchiang7/v4')
+      .then(response => response.json())
+      .then(json => {
+        const { stargazers_count, forks_count } = json;
+        setGitHubInfo({
+          stars: stargazers_count,
+          forks: forks_count,
+        });
+      })
+      .catch(e => console.error(e));
+  }, []);
+}
+
 function App(){
   return (
    <div id="Mainpage-1" className="Mainpage-1">
@@ -22,16 +48,26 @@ function App(){
     <Projects />
     <Footer />
     <div className="credits-container">
-    Designed and Developed by <br></br> <span className="owner-name"> Vivek Singh </span><img id="owner-github-link" src="src/assets/github-142-svgrepo-com.svg"></img><br></br>
-    </div>
+
+    <a href="https://github.com/bchiang7/v4">
+
+          <div>Designed &amp; Built by Brittany Chiang</div>
+
+          {githubInfo.stars && githubInfo.forks && (
+            <div className="github-stats">
+              <span>
+                <Icon name="Star" />
+                <span>{githubInfo.stars.toLocaleString()}</span>
+              </span>
+              <span>
+                <Icon name="Fork" />
+                <span>{githubInfo.forks.toLocaleString()}</span>
+              </span>
+              </div>
+
+    </a>  
    </div>
   );
-
-  // function nightMode(){
-  //   document.body.style.backgroundColor='#152744';
-  //   document.body.style.color='#FFF';
-  // }
-
 }
 
 export default App;
