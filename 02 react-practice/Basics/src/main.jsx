9e1e376fa31eb5d38f03355e2ec2fe349adae7cf
bettercodevivek <viveksingh0520@gmail.com>
2024-04-20 12:03:37 +0530

    Updation

--- 02 react-practice/Basics/src/main.jsx
+++ 02 react-practice/Basics/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import Vivek from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
   <Vivek/>
+  </React.StrictMode>
 )
 
