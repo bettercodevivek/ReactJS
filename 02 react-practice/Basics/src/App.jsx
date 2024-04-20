10075bfd0e25fc59d8fa0381a3017ac16321ec49
bettercodevivek <viveksingh0520@gmail.com>
2024-04-20 11:44:53 +0530

    Updation in  new directory

--- 02 react-practice/Basics/src/App.jsx
+++ 02 react-practice/Basics/src/App.jsx
@@ -1,3 +1,14 @@
 import './App.css'
 
+const Vivek = ()=>{
+  return (
+    <div id='mainpage'>
+       <a href='#'>Home</a>
+       <a href='#'>About Us</a>
+       <a href='#'>Contact Us</a>
+       <a href='#'>Gallery</a>
+    </div>
+  );
+}
 
+export default Vivek ;
