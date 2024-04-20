0f5426cae5e70c82d01292d5cc4b03ed69813877
bettercodevivek <viveksingh0520@gmail.com>
2024-04-20 13:16:09 +0530

    Updation

--- 02 react-practice/Basics/src/main.jsx
+++ 02 react-practice/Basics/src/main.jsx
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Vivek from "./App";
 
+
+// const reactElement= {
+//   type: "a",
+//   props:{
+//       href:'www.google.com',
+//       target:'_blank'
+//   },
+//   children:'click me to visit google'
+// }
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://www.google.com',target:'_blank'},
+  "mujhe click karoooooo!!!!!!"
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  <Vivek/>
-  </React.StrictMode>
+  
+  reactElement
 )
 
--- 02 react-practice/CustomReact/Custom.js
+++ 02 react-practice/CustomReact/Custom.js
@@ -1,10 +1,21 @@
-function customRender(param1 , param2){
-    const domElement = document.createElement(reactElement.type);
-    domElement.innerHTML=reactElement.children;
-    domElement.setAttribute('href',reactElement.props.href);
-    domElement.setAttribute('target',reactElement.props.target);
+function customRender(element , container){
+    // const domElement = document.createElement(reactElement.type);
+    // domElement.innerHTML=reactElement.children;
+    // domElement.setAttribute('href',reactElement.props.href);
+    // domElement.setAttribute('target',reactElement.props.target);
 
-    param2.appendChild(domElement);
+    // param2.appendChild(domElement);
+
+    const domElement=document.createElement(reactElement.type);
+
+    domElement.innerHTML=reactElement.children;
+
+     for (const prop in reactElement.props) {
+
+            domElement.setAttribute(prop,reactElement.props[prop]);
+     }
+
+    container.appendChild(domElement);
 }
 
 const reactElement= {
