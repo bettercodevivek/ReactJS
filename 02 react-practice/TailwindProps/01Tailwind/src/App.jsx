f4551670a38f6f99e11b17b1f88a617417dc43a9
bettercodevivek <viveksingh0520@gmail.com>
2024-04-25 21:47:12 +0530

    Updation

--- 02 react-practice/TailwindProps/01Tailwind/src/App.jsx
+++ 02 react-practice/TailwindProps/01Tailwind/src/App.jsx
@@ -4,13 +4,15 @@ import './App.css'
 function App() {
   return (
     <>
-    <div class='flex flex-row justify-evenly align-middle bg-teal-500 font-medium fixed top-0 left-0 w-screen' >
+    <div class='flex flex-row justify-evenly items-center bg-teal-500 font-medium fixed top-0 left-0 w-screen h-12' >
       <a href='#'>Home</a>
       <a href='#'>Home</a>
       <a href='#'>Home</a>
       <a href='#'>Home</a>
       <a href='#'>Home</a>
     </div>
+
+    <input type='checkbox' class='accent-orange-600 relative top-10' ></input>
     </>
   )
 }
