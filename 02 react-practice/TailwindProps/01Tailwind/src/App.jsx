3f3e45bda3353e039ce087c250348ca43c8c0555
bettercodevivek <viveksingh0520@gmail.com>
2024-04-25 18:12:36 +0530

    Updation

--- 02 react-practice/TailwindProps/01Tailwind/src/App.jsx
+++ 02 react-practice/TailwindProps/01Tailwind/src/App.jsx
@@ -4,9 +4,13 @@ import './App.css'
 function App() {
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div class='flex flex-row justify-evenly align-middle bg-teal-500 font-medium fixed top-0 left-0 w-screen' >
+      <a href='#'>Home</a>
+      <a href='#'>Home</a>
+      <a href='#'>Home</a>
+      <a href='#'>Home</a>
+      <a href='#'>Home</a>
+    </div>
     </>
   )
 }
