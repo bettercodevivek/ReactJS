e9f42ded2165743c6caab46a2abc7872a576e8ac
bettercodevivek <viveksingh0520@gmail.com>
2024-04-26 14:05:09 +0530

    updation

--- 02 react-practice/BGcolor-changer/src/App.jsx
+++ 02 react-practice/BGcolor-changer/src/App.jsx
@@ -1,20 +1,24 @@
 import './App.css'
 
 function App() {
-
+  const colorchanger=(id)=>{
+        if(id=='red'){
+           document.getElementById('mainpage').style.backgroundColor='red';
+        }
+  }
 
   return (
     <div className='mainpage'>
      
      <div id='footer' class='flex flex-row justify-evenly items-center bg-zinc-500 p-4 rounded-3xl shadow-md'>
-      <button id='red'>red</button>
-      <button id='blue'>blue</button>
-      <button id='green'>green</button>
-      <button id='violet'>violet</button>
-      <button id='pink'>pink</button>
-      <button id='black'>black</button>
-      <button id='yellow'>yellow</button>
-      <button id='teal'>teal</button>
+      <button id='red' class='bg-red-700 p-4 rounded-xl' onClick={colorchanger('red')}>red</button>
+      <button id='blue' class='bg-blue-600 p-4 rounded-xl'>blue</button>
+      <button id='green' class='bg-green-600 p-4 rounded-xl'>green</button>
+      <button id='violet' class='bg-violet-700 p-4 rounded-xl'>violet</button>
+      <button id='pink' class='bg-pink-700 p-4 rounded-xl'>pink</button>
+      <button id='black' class='bg-black text-zinc-50 p-4 rounded-xl'>black</button>
+      <button id='yellow' class='bg-yellow-400 p-4 rounded-xl'>yellow</button>
+      <button id='teal' class='bg-teal-600 p-4 rounded-xl'>teal</button>
      </div>
 
     </div>
