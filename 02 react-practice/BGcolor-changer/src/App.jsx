027b1163aa43aa3b238e69df8f7bd8ae1d657775
bettercodevivek <viveksingh0520@gmail.com>
2024-04-26 13:53:57 +0530

    Updation

--- 02 react-practice/BGcolor-changer/src/App.jsx
+++ 02 react-practice/BGcolor-changer/src/App.jsx
@@ -1,13 +1,24 @@
-
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
+    <div className='mainpage'>
+     
+     <div id='footer' class='flex flex-row justify-evenly items-center bg-zinc-500 p-4 rounded-3xl shadow-md'>
+      <button id='red'>red</button>
+      <button id='blue'>blue</button>
+      <button id='green'>green</button>
+      <button id='violet'>violet</button>
+      <button id='pink'>pink</button>
+      <button id='black'>black</button>
+      <button id='yellow'>yellow</button>
+      <button id='teal'>teal</button>
+     </div>
+
+    </div>
     
-    </>
   )
 }
 
