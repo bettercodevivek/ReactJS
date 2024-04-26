7e5099f0408f8e01a23477280fcb3917b0fcc868
bettercodevivek <viveksingh0520@gmail.com>
2024-04-26 17:58:35 +0530

    Updation

--- 02 react-practice/BGcolor-changer/src/App.jsx
+++ 02 react-practice/BGcolor-changer/src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
-
 function App() {
-  const colorchanger=(id)=>{
-        if(id=='red'){
-           document.getElementById('mainpage').style.backgroundColor='red';
-        }
-  }
+
 
   return (
     <div className='mainpage'>
      
      <div id='footer' class='flex flex-row justify-evenly items-center bg-zinc-500 p-4 rounded-3xl shadow-md'>
-      <button id='red' class='bg-red-700 p-4 rounded-xl' onClick={colorchanger('red')}>red</button>
+      <button id='red' class='bg-red-700 p-4 rounded-xl'>red</button>
       <button id='blue' class='bg-blue-600 p-4 rounded-xl'>blue</button>
       <button id='green' class='bg-green-600 p-4 rounded-xl'>green</button>
       <button id='violet' class='bg-violet-700 p-4 rounded-xl'>violet</button>
