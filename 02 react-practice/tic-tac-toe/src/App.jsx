485eee55d50ce0819604e5203975a5c4b84f618f
bettercodevivek <viveksingh0520@gmail.com>
2024-04-27 11:49:10 +0530

    Updation

--- 02 react-practice/tic-tac-toe/src/App.jsx
+++ 02 react-practice/tic-tac-toe/src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+function Square({value}){
+  return(
+   <button className='square'>{value}</button>
+  );
+}
 
 function App() {
 
   return (
     <>
+    <div id='container'>
+      <div id='row-1'>
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div id='row-2'>
+      <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div id='row-3'>
+      <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
+    </div>
   
     </>
   )
