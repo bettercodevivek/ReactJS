5fcfb4a7ec189524f46d81b8f73314de2a0c5788
bettercodevivek <viveksingh0520@gmail.com>
2024-05-02 20:02:22 +0530

    Updation

--- 01 react-practice/src/App.jsx
+++ 01 react-practice/src/App.jsx
@@ -54,47 +54,50 @@ function About({majorName,languages}){
   );
 }
 
-function Skills(){
+function Skills({urlforskilllogo}){
   return(
   <div id='Skills' className='Skills-mainpage'>
       <h1 className='Skills-heading'>MY SKILLS</h1>
       <div id="line2"></div>
       <div className='skills-container'>
           <div className='skill-item'>
-              <img className="icon" src="src\assets\git-svgrepo-com.svg " alt="GIT"></img>
+              <img className="icon" src={urlforskilllogo} alt="GIT"></img>
+              </div>
+              <div className='skill-item'>
+              <img className="icon" src={urlforskilllogo} alt="GIT"></img>
               </div>
 
               <div className='skill-item'>
-              <img className="icon" src="src\assets\python-svgrepo-com.svg " alt="GIT"></img>
+              <img className="icon" src={urlforskilllogo} alt="GIT"></img>
               </div>
 
               <div className='skill-item'>
-              <img className="icon" src="src\assets\cpp-svgrepo-com.svg " alt="GIT"></img>
+              <img className="icon" src={urlforskilllogo} alt="GIT"></img>
               </div>
 
               <div className='skill-item'>
-              <img className="icon" src="src\assets\css-3-svgrepo-com.svg " alt="GIT"></img>
+              <img className="icon" src={urlforskilllogo} alt="GIT"></img>
               </div>
 
               <div className='skill-item'>
-              <img className="icon" src="src\assets\html-5-svgrepo-com.svg " alt="GIT"></img>
+              <img className="icon" src={urlforskilllogo} alt="GIT"></img>
               </div>
 
               <div className='skill-item'>
-              <img className="icon" src="src\assets\java-svgrepo-com.svg " alt="GIT"></img>
+              <img className="icon" src={urlforskilllogo} alt="GIT"></img>
               </div>
 
               <div className='skill-item'>
-              <img className="icon" src="src\assets\js-svgrepo-com.svg " alt="GIT"></img>
+              <img className="icon" src={urlforskilllogo} alt="GIT"></img>
               </div>
 
               <div className='skill-item'>
-              <img className="icon" src="src\assets\node-js-svgrepo-com.svg " alt="GIT"></img>
+              <img className="icon" src={urlforskilllogo} alt="GIT"></img>
               </div>
 
-              <div className='skill-item'>
-              <img className="icon" src="src\assets\react-svgrepo-com.svg " alt="GIT"></img>
-              </div>
+
+
+             
       </div>
   </div>
   );
