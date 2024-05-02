abfa296d2ec1b0b0fd8c305be332cb0560287002
bettercodevivek <viveksingh0520@gmail.com>
2024-05-02 19:59:34 +0530

    Updation

--- 01 react-practice/src/App.jsx
+++ 01 react-practice/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Skills from "./Skills";
 import Projects from "./Projects";
 import Footer from "./Footer";
 import "./App.css";
@@ -55,6 +54,51 @@ function About({majorName,languages}){
   );
 }
 
+function Skills(){
+  return(
+  <div id='Skills' className='Skills-mainpage'>
+      <h1 className='Skills-heading'>MY SKILLS</h1>
+      <div id="line2"></div>
+      <div className='skills-container'>
+          <div className='skill-item'>
+              <img className="icon" src="src\assets\git-svgrepo-com.svg " alt="GIT"></img>
+              </div>
+
+              <div className='skill-item'>
+              <img className="icon" src="src\assets\python-svgrepo-com.svg " alt="GIT"></img>
+              </div>
+
+              <div className='skill-item'>
+              <img className="icon" src="src\assets\cpp-svgrepo-com.svg " alt="GIT"></img>
+              </div>
+
+              <div className='skill-item'>
+              <img className="icon" src="src\assets\css-3-svgrepo-com.svg " alt="GIT"></img>
+              </div>
+
+              <div className='skill-item'>
+              <img className="icon" src="src\assets\html-5-svgrepo-com.svg " alt="GIT"></img>
+              </div>
+
+              <div className='skill-item'>
+              <img className="icon" src="src\assets\java-svgrepo-com.svg " alt="GIT"></img>
+              </div>
+
+              <div className='skill-item'>
+              <img className="icon" src="src\assets\js-svgrepo-com.svg " alt="GIT"></img>
+              </div>
+
+              <div className='skill-item'>
+              <img className="icon" src="src\assets\node-js-svgrepo-com.svg " alt="GIT"></img>
+              </div>
+
+              <div className='skill-item'>
+              <img className="icon" src="src\assets\react-svgrepo-com.svg " alt="GIT"></img>
+              </div>
+      </div>
+  </div>
+  );
+}
 
 function App(){
   return (
