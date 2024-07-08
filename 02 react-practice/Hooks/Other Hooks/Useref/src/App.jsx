5115bc487b82094befbcb08d5b32073fad9d10fb
bettercodevivek <viveksingh0520@gmail.com>
2024-07-08 17:39:08 +0530

    Updation

--- 02 react-practice/Hooks/Other Hooks/Useref/src/App.jsx
+++ 02 react-practice/Hooks/Other Hooks/Useref/src/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState,useRef } from 'react';
 
-const App=()=>{
-  return(
-    <>
-    </>
+function Stopwatch(){
 
-  );
+const [Seconds,SetSeconds]=useState(0);
+const intervalRef=useRef(null);
+
+const Start =()=>{
+  if(!intervalRef.current){
+    intervalRef.current=setInterval(()=>{
+      SetSeconds(prevSeconds=>prevSeconds+1);
+    },1000);
+  }
+};
+
+const Stop = () => {
+  if (intervalRef.current) {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  }
+};
+
+ return(
+<>
+<h1>Seconds:{Seconds}</h1>
+<button onClick={Start}>Start</button>
+<button onClick={Stop}>Stop</button>
+</>
+ );
 }
 
-export default App;
+export default Stopwatch;
