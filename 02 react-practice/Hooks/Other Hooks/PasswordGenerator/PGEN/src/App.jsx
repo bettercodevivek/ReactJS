5505f13c17a0327be4b8d0a0c57878abad3fe7c1
bettercodevivek <viveksingh0520@gmail.com>
2024-07-11 20:00:27 +0530

    Updation

--- 02 react-practice/Hooks/Other Hooks/PasswordGenerator/PGEN/src/App.jsx
+++ 02 react-practice/Hooks/Other Hooks/PasswordGenerator/PGEN/src/App.jsx
@@ -1,66 +1,68 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+function App(){
 
 
-function App() {
+const [length,setlength]=useState(8);
+const [Password,setPassword]=useState("");
+const[numallowed,setnumallowed]=useState(false);
+const[charallowed,setcharallowed]=useState(false);
 
-  const[length,Setlength]=useState(8);
-  const[numallowed,Setnumallowed]=useState(false);
-  const[charallowed,Setcharallowed]=useState(false);
-  const[password,Setpassword]=useState("");
-  
-const passwordref=useRef(null);
+const Passref=useRef(null);
 
- const copytoclip = useCallback(()=>{
-  passwordref.current?.select();
-  window.navigator.clipboard.writeText(password);
- })
+const copybutton = useCallback(()=>{
+    Passref.current?.select();
+    window.navigator.clipboard.writeText(Password);
+})
 
-  const passwordgen = useCallback(()=>{
+const PasswordGenerator = useCallback(()=>{
+   
+console.log("Function working properly");
 
-     let pass=' ';
-     let string= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let Pass='';
+  let string='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-     if(numallowed){
-      string+='1234567890';
-     }
+  if(numallowed){
+    string+='1234567890';
+  }
 
-      if(charallowed){
-      string+='@#$%^&*';
-     }
+  if(charallowed){
+    string+='!@#$%^&*';
+  }
 
-     for(let i=1;i<=length;i++){
-      let char= Math.floor(Math.random()*string.length +1);
-      pass+=string.charAt(char);
-     }
+  for(let i=1;i<=length;i++){
+    let char=Math.floor(Math.random()*string.length + 1);
+    Pass+=string.charAt(char);
+  }
 
-     Setpassword(pass);
+   setPassword(Pass);
 
-  },[length,numallowed,charallowed,Setpassword]);
+},[length,numallowed,charallowed,setPassword]);
 
-   useEffect(()=>{
-      passwordgen();
-   },[length,numallowed,charallowed,Setpassword]);
-  return (
+useEffect(()=>{
+  PasswordGenerator();
+},[length,numallowed,charallowed,setPassword]);
+
+  return(
     <>
-      <h1 className='text-center text-3xl'>Password Generator</h1>
-      <div className=' relative max-w-md h-auto bg-green-500 rounded-xl shadow-lg'>
-        <div>
-          <input className='px-4 py-4 relative left-8 ' readOnly ref={passwordref} placeholder='enter your password' value={password} type='text'></input>
-          <button onClick={copytoclip} className='bg-black relative left-8 text-white px-4 py-4'>copy</button>
-        </div>
-        <div className=' '>
-          <input onChange={(e)=>{Setlength(e.target.value)}} type='range' min={8} max={50} value={length} className='cursor-pointer'/>
-          <label>length:{length}</label>
-        </div>
-        <div className='flex flex-row items-center gap-x-1'>
-          <input onChange={()=>{Setnumallowed((prev)=>!prev)}} id='numinput' defaultChecked={numallowed} type='checkbox'/>
-          <label htmlFor='charinput'>Numbers</label>
-          <input onChange={()=>{Setcharallowed((prev)=>!prev)}} id='charinput' defaultChecked={charallowed} type='checkbox'/>
-          <label htmlFor='charinput'>Characters</label>
-        </div>
+    <h1 className="text-center text-5xl">Password Generator</h1>
+    <div className="bg-gray-500 flex flex-row items-center justify-center relative top-48 max-w-sm min-h-lg">
+      <div>
+        <input ref={Passref} type="text" readOnly value={Password}/>
+        <button onClick={copybutton}>Copy</button>
       </div>
+      <div>
+        <input value={length} type='range' min={8} max={30} onChange={(e)=>{setlength(e.target.value)}}/>
+        <label>length:{length}</label>
+        <input id="numid" type="checkbox" onChange={()=>{setnumallowed((prev)=>!prev)}}/>
+        <label htmlFor="numid">Number</label>
+        <input id="charid" type="checkbox" onChange={()=>{setcharallowed((prev)=>!prev)}}/>
+        <label htmlFor="charid">Character</label>
+      </div>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+
+export default App;
