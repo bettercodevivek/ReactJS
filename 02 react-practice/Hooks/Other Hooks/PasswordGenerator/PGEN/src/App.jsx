f21afb872dce51c6cd7154866732a4aaed1227d3
bettercodevivek <viveksingh0520@gmail.com>
2024-07-11 14:09:36 +0530

    Updation

--- 02 react-practice/Hooks/Other Hooks/PasswordGenerator/PGEN/src/App.jsx
+++ 02 react-practice/Hooks/Other Hooks/PasswordGenerator/PGEN/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 
 function App() {
@@ -6,6 +6,29 @@ function App() {
   const[length,Setlength]=useState(8);
   const[numallowed,Setnumallowed]=useState(false);
   const[charallowed,Setcharallowed]=useState(false);
+  const[password,Setpassword]=useState("");
+
+  const passwordgen = useCallback(()=>{
+
+     let pass=' ';
+     let string= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+     if(numallowed){
+      string+='1234567890';
+     }
+
+      if(charallowed){
+      string+='@#$%^&*';
+     }
+
+     for(let i=1;i<=length;i++){
+      let char= Math.floor(Math.random()*string.length +1);
+      pass=string.charAt(char);
+     }
+
+     Setpassword(pass);
+
+  },[length,numallowed,charallowed,Setpassword])
 
   return (
     <>
