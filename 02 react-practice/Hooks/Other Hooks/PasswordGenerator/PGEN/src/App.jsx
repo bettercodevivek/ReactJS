0841ad7a21aaa77913ba4c18b60bcdfd4e4143f7
bettercodevivek <viveksingh0520@gmail.com>
2024-07-11 13:46:13 +0530

    updation

--- 02 react-practice/Hooks/Other Hooks/PasswordGenerator/PGEN/src/App.jsx
+++ 02 react-practice/Hooks/Other Hooks/PasswordGenerator/PGEN/src/App.jsx
@@ -3,10 +3,13 @@ import { useState } from 'react'
 
 function App() {
 
+  const[length,Setlength]=useState(8);
+  const[numallowed,Setnumallowed]=useState(false);
+  const[charallowed,Setcharallowed]=useState(false);
 
   return (
     <>
-      
+      <h1 className='text-center text-3xl'>Password Generator</h1>
     </>
   )
 }
