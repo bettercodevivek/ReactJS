8ab051978cb8052a003bc3b75e75e7ebcea4c3af
bettercodevivek <viveksingh0520@gmail.com>
2024-07-17 19:16:05 +0530

    Updation

--- 02 react-practice/Context api/context/src/App.jsx
+++ 02 react-practice/Context api/context/src/App.jsx
@@ -1,11 +1,12 @@
+import UserContextProvider from "./Context/UserContextProvider"
 
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
     <h1 className="text-3xl text-center">Context API</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
