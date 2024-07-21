1755c31bcaf7ff9290d2472840120fec72f05a5f
bettercodevivek <viveksingh0520@gmail.com>
2024-07-21 12:03:18 +0530

    Updation in project

--- 02 react-practice/Context api/context/src/App.jsx
+++ 02 react-practice/Context api/context/src/App.jsx
@@ -1,11 +1,13 @@
 import UserContextProvider from "./Context/UserContextProvider"
-
-
+import Login from "./Components/Login"
+import Profile from "./Components/Profile"
 function App() {
 
   return (
     <UserContextProvider>
     <h1 className="text-3xl text-center">Context API !!!!</h1>
+    <Login/>
+    <Profile/>
     </UserContextProvider>
   )
 }
