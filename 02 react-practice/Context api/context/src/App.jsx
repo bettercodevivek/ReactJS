82d63a9d77f370c171ac5c095130deefbda63f6b
bettercodevivek <viveksingh0520@gmail.com>
2024-07-21 12:27:35 +0530

    Updation

--- 02 react-practice/Context api/context/src/App.jsx
+++ 02 react-practice/Context api/context/src/App.jsx
@@ -1,14 +1,7 @@
-import UserContextProvider from "./Context/UserContextProvider"
-import Login from "./Components/Login"
-import Profile from "./Components/Profile"
 function App() {
 
   return (
-    <UserContextProvider>
-    <h1 className="text-3xl text-center">Context API !!!!</h1>
-    <Login/>
-    <Profile/>
-    </UserContextProvider>
+   <h1 className="text-5xl text-center text-semibold"> Theme Changer using context api</h1>
   )
 }
 
